fbc277df6e32e80cabd92952427dbcc1749922ea
JactusTheCactus (PC) <threateninglyamusing@gmail.com>
2025-11-30 05:54:38 -0800

    -

--- .sh
+++ .sh
@@ -10,7 +10,9 @@ DIRS=(
 	bin
 )
 for i in "${DIRS[@]}"; do
+	rm -rf "$i" || :
 	mkdir -p "$i"
 done
 exec > logs/app.log
 find . -name "*.apk" -exec cp {} bin \;
+find . -name "*.gradle" -exec echo {} \;
